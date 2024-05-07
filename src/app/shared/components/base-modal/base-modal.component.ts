25514df4bc0066343dd964a3f083d25c74d06a9d
Mirco Ish <mirco.df@live.it>
2024-05-07 14:06:05 +0200

    Base modal component init | WIP

---
+++ src/app/shared/components/base-modal/base-modal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngkdx-base-modal',
+  templateUrl: './base-modal.component.html',
+  styleUrls: ['./base-modal.component.scss']
+})
+export class BaseModalComponent {
+
+}
--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -4,13 +4,15 @@ import { HeaderComponent } from './header/header.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
 import { WaitingComponent } from './waiting/waiting.component';
+import { BaseModalComponent } from './base-modal/base-modal.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    WaitingComponent
+    WaitingComponent,
+    BaseModalComponent
   ],
   imports: [
     CommonModule,
