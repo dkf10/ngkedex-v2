0df71631570665257e2b42a9346db9cd1d43b79c
Mirco Ish <mirco.df@live.it>
2024-05-16 15:43:12 +0200

    Search in about fix && removed learn more

--- src/app/pages/about/about-routing.module.ts
+++ src/app/pages/about/about-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutLandingComponent } from './components/landing/landing.component';
+import { aboutResolver } from 'src/app/core/resolvers/about/about.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    component: AboutLandingComponent
+    component: AboutLandingComponent,
+    resolve: { data: aboutResolver }
   }
 ];
 
