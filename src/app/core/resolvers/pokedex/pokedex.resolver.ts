d6c02e1682a0ea42b0380f8828e9ce96b4183821
Mirco Ish <mirco.df@live.it>
2024-05-16 15:19:42 +0200

    Hidden WIP features

--- src/app/core/resolvers/pokedex/pokedex.resolver.ts
+++ src/app/core/resolvers/pokedex/pokedex.resolver.ts
@@ -2,11 +2,11 @@ import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { of } from 'rxjs';
 import { SearchService } from 'src/app/shared/services/search/search.service';
-import { mainMenu } from '../../enum/routes.enum';
 
-export const pokedexResolver: ResolveFn<boolean> = (route, state) => {
+export const pokedexResolver: ResolveFn<boolean> = () => {
   const searchService = inject(SearchService);
-  searchService.searchBtnVisible = (state.url === `/${mainMenu.POKEDEX}`);
+  // searchService.searchBtnVisible = (state.url === `/${mainMenu.POKEDEX}`);
+  searchService.searchBtnVisible = false;
   searchService.searchTerm = null;
   return of(true);
 };
