8ff782969a9136fa1ee10312490e12448008991c
Mirco Ish <mirco.df@live.it>
2024-05-16 16:33:55 +0200

    Restored hidden functions

--- src/app/core/resolvers/pokedex/pokedex.resolver.ts
+++ src/app/core/resolvers/pokedex/pokedex.resolver.ts
@@ -2,11 +2,11 @@ import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { of } from 'rxjs';
 import { SearchService } from 'src/app/shared/services/search/search.service';
+import { mainMenu } from '../../enum/routes.enum';
 
-export const pokedexResolver: ResolveFn<boolean> = () => {
+export const pokedexResolver: ResolveFn<boolean> = (route, state) => {
   const searchService = inject(SearchService);
-  // searchService.searchBtnVisible = (state.url === `/${mainMenu.POKEDEX}`);
-  searchService.searchBtnVisible = false;
+  searchService.searchBtnVisible = (state.url === `/${mainMenu.POKEDEX}`);
   searchService.searchTerm = null;
   return of(true);
 };
